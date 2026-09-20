3f77adb2c86d543b04391400176405f84169ae34
Anuradha <ee21resch01008@iith.ac.in>
2026-09-20 23:38:12 +0530

    First commit

---
+++ vpr/utils/losses.py
@@ -0,0 +1,30 @@
+from pytorch_metric_learning import losses, miners
+from pytorch_metric_learning.distances import CosineSimilarity, DotProductSimilarity
+#from pytorch_metric_learning.losses.multi_similarity_loss_p2p import MultiSimilarityLoss_P2P
+#from pytorch_metric_learning.losses.lifted_structure_loss_p2p import GeneralizedLiftedStructureLoss
+#from pytorch_metric_learning.losses.ntxent_loss_p2p import NTXentLoss
+
+def get_loss(loss_name):
+    if loss_name == 'SupConLoss': return losses.SupConLoss(temperature=0.07)
+    if loss_name == 'CircleLoss': return losses.CircleLoss(m=0.4, gamma=80) #these are params for image retrieval
+    if loss_name == 'MultiSimilarityLoss': return losses.MultiSimilarityLoss(alpha=1.0, beta=50, base=0.0, distance=DotProductSimilarity())
+    #if loss_name == 'MultiSimilarityLoss': return MultiSimilarityLoss_P2P(alpha=1.0, beta=50, base=0.0, distance=DotProductSimilarity())
+    if loss_name == 'ContrastiveLoss': return losses.ContrastiveLoss(pos_margin=0, neg_margin=1)
+    #if loss_name == 'Lifted': return losses.GeneralizedLiftedStructureLoss(neg_margin=0, pos_margin=1, distance=DotProductSimilarity())
+    if loss_name == 'Lifted': return GeneralizedLiftedStructureLoss(neg_margin=0, pos_margin=1, distance=DotProductSimilarity())
+    if loss_name == 'FastAPLoss': return losses.FastAPLoss(num_bins=30)
+    if loss_name == 'NPair': return losses.NPairsLoss()
+    if loss_name == 'NTXentLoss': return losses.NTXentLoss(temperature=0.07) #The MoCo paper uses 0.07, while SimCLR uses 0.5.
+    if loss_name == 'NTXentLoss_P2P': return NTXentLoss(temperature=0.07) #The MoCo paper uses 0.07, while SimCLR uses 0.5.
+    if loss_name == 'TripletMarginLoss': return losses.TripletMarginLoss(margin=0.1, swap=False, smooth_loss=False, triplets_per_anchor='all') #or an int, for example 100
+    if loss_name == 'CentroidTripletLoss': return losses.CentroidTripletLoss(margin=0.05,
+                                                                            swap=False,
+                                                                            smooth_loss=False,
+                                                                            triplets_per_anchor="all",)
+    raise NotImplementedError(f'Sorry, <{loss_name}> loss function is not implemented!')
+
+def get_miner(miner_name, margin=0.1):
+    if miner_name == 'TripletMarginMiner' : return miners.TripletMarginMiner(margin=margin, type_of_triplets="semihard") # all, hard, semihard, easy
+    if miner_name == 'MultiSimilarityMiner' : return miners.MultiSimilarityMiner(epsilon=margin, distance=CosineSimilarity())
+    if miner_name == 'PairMarginMiner' : return miners.PairMarginMiner(pos_margin=0.7, neg_margin=0.3, distance=DotProductSimilarity())
+    return None
